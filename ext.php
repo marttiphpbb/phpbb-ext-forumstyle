b68a69fea056d4a63323ed6936274516e7e42a20
martti <info@martti.be>
2018-09-26 14:33:33 +0200

    init

---
+++ ext.php
@@ -0,0 +1,22 @@
+<?php
+/**
+* phpBB Extension - marttiphpbb forumstyle
+* @copyright (c) 2018 marttiphpbb <info@martti.be>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace marttiphpbb\forumstyle;
+
+use phpbb\extension\base;
+
+class ext extends base
+{
+	/**
+	 * phpBB 3.2.1+ and PHP 7.1+
+	 */
+	public function is_enableable()
+	{
+		$config = $this->container->get('config');
+		return phpbb_version_compare($config['version'], '3.2.1', '>=') && version_compare(PHP_VERSION, '7.1', '>=');
+	}
+}
---
+++ util/cnst.php
@@ -0,0 +1,19 @@
+<?php
+/**
+* phpBB Extension - marttiphpbb Forum Style
+* @copyright (c) 2018 marttiphpbb <info@martti.be>
+* @license GNU General Public License, version 2 (GPL-2.0)
+*/
+
+namespace marttiphpbb\forumstyle\util;
+
+class cnst
+{
+	const FOLDER = 'marttiphpbb/forumstyle';
+	const ID = 'marttiphpbb_forumstyle';
+	const PREFIX = self::ID . '_';
+	const L = 'MARTTIPHPBB_FORUMSTYLE';
+	const L_ACP = 'ACP_' . self::L;
+	const L_MCP = 'MCP_' . self::L;
+	const TPL = '@' . self::ID . '/';
+}
