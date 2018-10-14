d0fd640edc416d7895f9d39d047bf9d1b99e9b84
martti <info@martti.be>
2018-10-14 20:22:39 +0200

    forumlist

--- styles/prosilver/template/js/viewforum.js
+++ styles/prosilver/template/js/viewforum.js
@@ -1,5 +1,5 @@
 ;(function($, window, document) {
 	$('document').ready(function(){
-		$('div#page-body').addClass('forum-' + $('span#marttiphpbb-forumstyle').data('forum-id'));
+		$('div.forumbg').addClass('forum-' + $('span#marttiphpbb-forumstyle').data('forum-id'));
 	});
 })(jQuery, window, document);
